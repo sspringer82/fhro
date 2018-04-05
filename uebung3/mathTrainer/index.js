c8c5fe5fc1b1112d14bb4ddb95bb9a829f24e703
Sebastian Springer <info@sebastian-springer.com>
2018-04-05 18:33:16 +0200

    intermediate commit

--- uebung3/mathTrainer/index.js
+++ uebung3/mathTrainer/index.js
@@ -6,13 +6,16 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const task = new Task('-');
+//const task = new Task('-');
 
-rl.question(task.toString(), answer => {
-  debugger;
-  if (parseInt(answer, 10) === task.result) {
-    console.log('richtig!');
-  } else {
-    console.log('falsch');
+const operands = ['+', '-', '*', '/'];
+
+(async () => {
+  for (let i = 0; i < operands.length; i++) {
+    for (let j = 0; j < 4; j++) {
+      const task = new Task(operands[i]);
+      await task.ask(rl);
+    }
   }
-});
+  rl.close();
+})();
--- uebung3/mathTrainer/tasks.js
+++ uebung3/mathTrainer/tasks.js
@@ -1,7 +1,13 @@
 module.exports = class Task {
   constructor(operator) {
-    this.operand1 = Math.floor(Math.random() * 10);
-    this.operand2 = Math.floor(Math.random() * 10);
+    if (operator === '/') {
+      this.operand1 = Math.floor(Math.random() * 10);
+      this.operand2 = Math.floor(Math.random() * 10);
+      this.operand1 = this.operand1 * this.operand2;
+    } else {
+      this.operand1 = Math.floor(Math.random() * 10);
+      this.operand2 = Math.floor(Math.random() * 10);
+    }
     this.operator = operator;
     const obj = {
       '+': this.operand1 + this.operand2,
@@ -14,5 +20,18 @@ module.exports = class Task {
 
   toString() {
     return `${this.operand1} ${this.operator} ${this.operand2} = `;
+  }
+
+  ask(rl) {
+    return new Promise((resolve, reject) => {
+      rl.question(this.toString(), answer => {
+        if (parseInt(answer, 10) === this.result) {
+          console.log('richtig!');
+        } else {
+          console.log('falsch');
+        }
+        resolve(true);
+      });
+    });
   }
 };
