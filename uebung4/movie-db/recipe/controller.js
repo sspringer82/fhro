c9f44c79e51b778938300710e5303197528f0851
Sebastian Springer <info@sebastian-springer.com>
2018-04-12 17:57:14 +0200

    delete stuff

--- uebung4/movie-db/recipe/controller.js
+++ uebung4/movie-db/recipe/controller.js
@@ -5,7 +5,22 @@ module.exports = class Controller {
   getAll(req, res) {
     this.db.get().then(
       recipes => {
-        res.render(__dirname + '/views/list', { recipes });
+        res.render(__dirname + '/views/list', {
+          recipes,
+          baseUrl: req.baseUrl,
+        });
+      },
+      err => {
+        console.log(err);
+      },
+    );
+  }
+
+  delete(req, res) {
+    const id = parseInt(req.params.id, 10);
+    this.db.delete(id).then(
+      data => {
+        res.redirect(req.baseUrl);
       },
       err => {
         console.log(err);
--- uebung4/movie-db/recipe/index.js
+++ uebung4/movie-db/recipe/index.js
@@ -4,19 +4,11 @@ const Controller = require('./controller');
 
 const router = express.Router();
 
-const recipes = new CsvDb('./db/recipe.csv', [
-  'id',
-  'title',
-  'time',
-  'rating',
-  'level',
-  'ingredients',
-  'manual',
-  'created',
-]);
+const recipes = new CsvDb('./db/recipe.csv');
 
 const controller = new Controller(recipes);
 
 router.get('/', (req, res) => controller.getAll(req, res));
+router.get('/delete/:id', (req, res) => controller.delete(req, res));
 
 module.exports = router;
