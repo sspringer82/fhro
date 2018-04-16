bbbade9c8a2d93d39601c73b18deadea0ecccafc
Sebastian Springer <info@sebastian-springer.com>
2018-04-16 19:30:05 +0200

    make controller stuff async

--- uebung4/recipe-db/recipe/controller.js
+++ uebung4/recipe-db/recipe/controller.js
@@ -2,47 +2,41 @@ module.exports = class Controller {
   constructor(db) {
     this.db = db;
   }
-  getAll(req, res) {
-    this.db.get().then(
-      recipes => {
-        res.render(__dirname + '/views/list', {
-          recipes,
-          baseUrl: req.baseUrl,
-        });
-      },
-      err => {
-        console.log(err);
-      },
-    );
-  }
-
-  delete(req, res) {
-    const id = parseInt(req.params.id, 10);
-    this.db.delete(id).then(
-      data => {
-        res.redirect(req.baseUrl);
-      },
-      err => {
-        console.log(err);
-      },
-    );
-  }
-
-  form(req, res) {
-    let promise;
-    if (req.params.id) {
-      promise = this.db.get(req.params.id);
-    } else {
-      promise = Promise.resolve({ id: null, title: null });
+  async getAll(req, res) {
+    try {
+      const recipes = await this.db.get();
+      res.render(__dirname + '/views/list', {
+        recipes,
+        baseUrl: req.baseUrl,
+      });
+    } catch (err) {
+      res.status = 500;
+      res.send(err);
     }
-    promise.then(recipe =>
-      res.render(__dirname + '/views/form', { baseUrl: req.baseUrl, recipe }),
-    );
+  }
+
+  async delete(req, res) {
+    try {
+      const id = parseInt(req.params.id, 10);
+      await this.db.delete(id);
+      res.redirect(req.baseUrl);
+    } catch (err) {
+      res.status = 500;
+      res.send(err);
+    }
+  }
+
+  async form(req, res) {
+    let recipe = { id: null, title: null };
+    if (req.params.id) {
+      recipe = await this.db.get(req.params.id);
+    }
+    res.render(__dirname + '/views/form', { baseUrl: req.baseUrl, recipe });
   }
 
   async save(req, res) {
     if (req.body.id) {
-      this.db.update(req.body, req.body.id);
+      await this.db.update(req.body, req.body.id);
     } else {
       await this.db.insert(req.body);
     }
